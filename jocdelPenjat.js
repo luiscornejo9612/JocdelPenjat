a6e26fedbe0677df8a7a723b2823ab197ab386e1
luis <lcornejo20@jaumebalmes.net>
2023-11-08 20:45:31 +0100

    vercion 3 la letra ya se pone en su posicion

--- jocdelPenjat.js
+++ jocdelPenjat.js
@@ -4,38 +4,37 @@ function jocdelPenjat() {
         if (Number.isInteger(num)) {
             if (num === 1) {
                 var palabra = prompt("Ingresa la palabra");
-                var longitud = palabra.length;
-                var numLetras = "";
-                for (let i = 0; i < longitud; i++) {
-                    numLetras+=" _";
+                var guiones = "";
+                var palabraConGuiones = "";   
+                for (let index = 0; index < palabra.length; index++) {
+                    palabraConGuiones += palabra[index] + " ";
                 }
-                console.log(numLetras)
+
+                for (let i = 0; i < palabra.length; i++) {
+                    guiones+="_"+" ";
+                }
+                console.log(guiones);
                 var contador = 0;
                 do {
-                    letra = prompt("Ingresa una letra");
-                
-                    if (letra.length !== 1 || !esLetra(letra)) {
+                    var letras = prompt("Ingresa una letra");
+                    var letrasArray = [];
+                    if (letras.length !== 1 || !esLetra(letras)) {
                         console.log("Ingresa una única letra válida que no sea un número.");
-                    }
-                    else{
-                        var exist = palabra.includes(letra);
-                        for (let i = 0; i < longitud; i++) {
-                            if (exist != false) {
-                                console.log(letra);
-                                
-                            }
-                        }
-                        contador++;
-                    }
-                    
-
+                    }else{
+                        letrasArray.push(letras);
+                        for (let i = 0; i < palabraConGuiones.length; i++) {
+                            if (palabraConGuiones[i] === letras) {
+                                var resultat = guiones.substring(0, i) + letras + guiones.substring(i+1);
+                                console.log(resultat);
+                            }else{
+                                console.log("La letra no se encuentra en la palabra");
+                                contador++;
+                            }          
+                        }    
+                    }                              
                 } while (contador != 6);
                 console.log("haz ganado");
-
-
-            }
-
-        
+            }       
         }else{
             console.log("ingrese una opción correcta");
         }
@@ -48,4 +47,12 @@ function esLetra(caracter) {
     var expresionRegular = /^[a-zA-Z]$/;
     // Usamos el método test para verificar si el caracter cumple con la expresión regular.
     return expresionRegular.test(caracter);
+}
+
+function name(letra, palabra, resultat) {
+    letra = "";
+    if (palabra[i] === letras) {
+        var resultat = guiones.substring(0, i) + letras + guiones.substring(i+1);
+        console.log(resultat);
+    }
 }
