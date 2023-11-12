9feb273ae6ed2048b186937fdfe689e53db2e8d9
= <lcornejo20@jaumebalmes.net>
2023-11-12 18:44:44 +0100

    vercion2 agregada, funciona toda la parte de jugar

---
+++ vercion2.js
@@ -0,0 +1,78 @@
+function jocdelPenjatv2() {
+    do {
+        var menu = parseInt(prompt("INGRESA UN NUMERO:" +
+            "\n1 = Jugar, " +
+            "\n2 = Estadisticas, " +
+            "\n3 = Salir"));
+        if (Number.isInteger(menu)) {
+            if (menu === 1) {
+                let palabra = prompt("Ingresa la palabra");
+                palabra = palabra.toUpperCase();
+                var palabraEspacios = "";
+                for (let index = 0; index < palabra.length; index++) {
+                    palabraEspacios += palabra[index] + " ";
+                }
+                let arrayDeLetras = palabraEspacios.split('');
+
+                var guiones = "";
+                for (let i = 0; i < palabra.length; i++) {
+                    guiones += "_" + " ";
+                }
+                console.log(guiones);
+                let gionesArray = guiones.split('');
+                var contador = 6;
+                var posiciones = []; // Almacena las posiciones de la letra encontrada
+                var palabraAdivinada = false;
+
+                do {
+                    var letra = prompt("Ingresa una letra");
+                    letra = letra.toUpperCase();
+                    if (letra.length !== 1 || !esLetra(letra)) {
+                        console.log("Solo se admiten letras");
+                    } else {
+                        var exist = arrayDeLetras.includes(letra);
+                        if (exist) {
+                            for (let i = 0; i < arrayDeLetras.length; i++) {
+                                if (arrayDeLetras[i] === letra) {
+                                    posiciones.push(i); // Almacena la posición
+                                    gionesArray[i] = letra;
+                                }
+                            }
+                            let texto = gionesArray.join('');
+                            console.log(texto);
+                            
+                            if (!gionesArray.includes('_')) {
+                                palabraAdivinada = true;
+                                console.log("¡HAS GANADO!")
+                                break; // Salir del bucle si se adivinó la palabra
+                            }
+                        }
+                        else {
+                            contador--;
+                            console.log("La letra no se encuentra en la palabra, te quedan " + contador + " intentos");
+                            if (contador === 0) {
+                                console.log("¡HAS PERDIDO!");     
+                            }
+                        }
+                    }
+                } while (contador !== 0 );
+            }
+        } else {
+            console.log("Ingrese una opcion correcta.");
+        }
+    } while (menu !== 3);
+}
+
+
+
+function esLetra(caracter) {
+    //verifica si el caracter es una letra (mayúscula o minúscula).
+    var expresionRegular = /^[a-zA-Z]$/;
+    // Usamos el método test para verificar si el caracter cumple con la expresión regular.
+    return expresionRegular.test(caracter);
+}
+
+
+
+
+
